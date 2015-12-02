b15f4b157b80bad1440f1ecaf1365ff47d0d9169
Gustav Nikolaj Olsen <gno@one.com>
2015-12-02 16:10:32 +0100

    don't directly export resolveConfig...

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-var when = require('when');
-var findNecessaryContainers = require('./lib/findNecessaryContainers');
-var createContainer = require('./lib/createContainer');
-
-function resolveConfigurationAndStartContainers (config, callback) {
-    if (!config.docker || !config.docker.enabled) {
-        return callback(null, config);
+module.exports = {
+    get resolveConfigurationAndStartContainers () {
+        return require('./lib/resolveConfigurationAndStartContainers');
     }
-
-    var containers = findNecessaryContainers(config);
-
-    if (containers.length === 0) {
-        return callback(null, config);
-    }
-
-    when.map(containers, function (containerSpec) {
-        return createContainer(config.docker.options, containerSpec);
-    }).then(function (containers) {
-        containers.forEach(function (container) {
-            var configKey = container.containerSpec.key;
-            var newValue = container.containerSpec.value;
-
-            for (var prop in newValue) {
-                var value = newValue[prop];
-                if (newValue.hasOwnProperty(prop) && typeof value === 'string') {
-                    value = value.replace(/\{docker_port\}/g, container.info.port);
-                    value = value.replace(/\{docker_host\}/g, container.info.ip);
-                    newValue[prop] = value.replace(/\{docker_env:([a-zA-Z_]+)\}/g, function (match, key) {
-                        return container.containerSpec.docker.env[key];
-                    });
-                }
-            }
-
-            config[configKey] = newValue;
-        });
-        callback(null, config);
-    }, callback);
 };
-
-module.exports = resolveConfigurationAndStartContainers;
