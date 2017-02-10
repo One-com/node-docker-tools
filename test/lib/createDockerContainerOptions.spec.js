29b6667c5ea587e1eb21cb31cf54b104a5ec88bc
Joel Mukuthu <jmu@one.com>
2017-02-10 12:06:57 +0100

    Add support for port bindings

--- test/lib/createDockerContainerOptions.spec.js
+++ test/lib/createDockerContainerOptions.spec.js
@@ -14,4 +14,39 @@ describe('lib/createDockerContainerOptions', function () {
             HostConfig: { PublishAllPorts: true }
         });
     });
+
+    it("should map 'hostPort' to PortBindings", function () {
+        return expect(createDockerContainerOptions({
+            image: 'redis',
+            port: 5432,
+            hostPort: 54321
+        }), 'to equal', {
+            Image: 'redis',
+            ExposedPorts: {
+                '5432/tcp': {}
+            },
+            HostConfig: {
+                PublishAllPorts: true,
+                PortBindings: {
+                    '5432/tcp': [{
+                        HostPort: '54321'
+                    }]
+                }
+            }
+        });
+    });
+
+    it("should not map 'hostPort' to PortBindings of 'port' has not been configured", function () {
+        return expect(createDockerContainerOptions({
+            image: 'redis',
+            hostPort: 54321
+        }), 'to equal', {
+            Image: 'redis',
+            ExposedPorts: undefined,
+            HostConfig: {
+                PublishAllPorts: true,
+                PortBindings: undefined
+            }
+        });
+    });
 });
